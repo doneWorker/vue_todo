7c4185651fcb654f19d008d5db6bd0a242b3ff42
Kirill Anikin <anikinkirill22@gmail.com>
2021-03-01 00:47:27 +0500

    add todo function added

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="todo-form">
+    <input type="text" v-model="title" placeholder="Your Task" />
+    <button id="add-todo" @click="submit">Add Todo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    submit() {
+      this.$parent.addTodo({ title: this.title });
+      this.title = "";
+    },
+  },
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="todo-list">
+    <li v-for="(todo, idx) in todos" :key="idx">
+      {{ todo.title }}
+    </li>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ["todos"],
+  data() {},
+};
+</script>
